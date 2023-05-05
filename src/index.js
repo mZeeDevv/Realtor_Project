3896ea2d64f3165a10bd3fe3e8c32e049e3235d1
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-05-05 22:28:17 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <>
+ <h1 className='bg-slate-400'>HELLO WORLDS</h1>
+ </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
