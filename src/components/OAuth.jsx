27b0944eaf81bd0d097e5aeea0cef94d9bcd0a34
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-05-11 15:33:38 +0500

    Completed auth process

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import {FcGoogle} from "react-icons/fc"
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { toast } from 'react-toastify';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import {db} from "../firebase"
+import { useNavigate } from 'react-router';
 
 export default function OAuth() {
+  const Navigate = useNavigate()
+ async function onGoogleClick(){
+try {
+  const auth = getAuth()
+  const provider = new GoogleAuthProvider()
+  const result = await signInWithPopup(
+    auth,
+    provider
+  )
+  const user = result.user;
+  const docRef = doc(db, "users", user.uid)
+  const docSnap = await getDoc(docRef)
+  if(!docSnap.exists()){
+ await setDoc(docRef, {
+  name: user.displayName,
+  email: user.email,
+  timestamp: serverTimestamp(),
+ })
+ Navigate("/")
+  }
+} catch (error) {
+  console.log(error)
+}
+  }
   return (
-    <button
+    <button type='button'
+    onClick={onGoogleClick}
     className='flex justify-center items-center w-full bg-red-700 text-white uppercase
     py-4 text-sm font-medium hover:bg-red-800 shadow-md hover:shadow-lg 
     transition duration-200 ease-in-out rounded'>
